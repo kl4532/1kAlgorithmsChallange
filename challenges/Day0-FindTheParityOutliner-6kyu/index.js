0571c0a2068cb8bd1b165913e031f552ad8d86a5
kl4532 <kornellukasiak@gmail.com>
2019-07-31 21:18:14 +0200

    delete comments

--- challenges/Day0-FindTheParityOutliner-6kyu/index.js
+++ challenges/Day0-FindTheParityOutliner-6kyu/index.js
@@ -1,10 +1,9 @@
 function findOutlier(integers){
-    //your code here
     let temp = 0;
     for(let i=0; i<3; i++){
       integers[i]%2 === 0 ? temp++ : temp;
     }
     return temp>1 ? integers.filter(i=>i%2!==0)[0] : integers.filter(i=>i%2==0)[0];
 }
-//console.log(findOutlier([2, 2, 2, 0, 1, 4]));
-//console.log(findOutlier([1, 1, 7, 1, 2, 1]));
+console.log(findOutlier([2, 2, 2, 0, 1, 4]));
+console.log(findOutlier([1, 1, 7, 1, 2, 1]));
