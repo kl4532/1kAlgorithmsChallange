3a5dea5d2d33c1de912dd5dcbbfda5b56c7be699
kl4532 <kornellukasiak@gmail.com>
2019-07-31 21:16:28 +0200

    layout bootstrap  grid

---
+++ app.js
@@ -0,0 +1,18 @@
+fetch("challanges.json")
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    let html="";
+    let bgcolor;
+    let odd = true;
+    json.challenges.map(o=>{
+      fetch(o.link)
+        .then(response => response.text())
+        .then(code => {
+          odd ? bgcolor="bg-dark" : bgcolor="bg-secondary";
+          odd ? odd = false : odd = true;
+          html += `<div class="row ${bgcolor} text-light"><div class="col-2">${o.id}</div><div class="col-4">${o.title}</div><div class="col-2"><font color="${o.kyu>=7 ? "white" : o.kyu<7 && o.kyu>4 ? "yellow" : "red"}">${o.kyu}</font></div><div data-toggle="collapse" href="#collapse${o.id}" role="button" aria-expanded="false" aria-controls="collapseExample" class="col-4 code-drop">&lt;/&gt;</div></div><div class="row collapse" id="collapse${o.id}"> <div class="col card card-body"> <pre class="prettyprint"> <code class="language-javascript" >${code}</code> </pre></div></div>`;
+          document.getElementById("bdy").innerHTML=html;
+        })
+    })
+  });
