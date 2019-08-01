388f403645a2a5f8036e202e2ad83f0f3c58f1db
kl4532 <kornellukasiak@gmail.com>
2019-08-01 10:58:01 +0200

    convert entities in js code

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ fetch("challanges.json")
       fetch(o.link)
         .then(response => response.text())
         .then(code => {
+          code = String(code).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
           odd ? bgcolor="bg-dark" : bgcolor="bg-secondary";
           odd ? odd = false : odd = true;
           html += `<div class="row ${bgcolor} text-light"><div class="col-2">${o.id}</div><div class="col-4">${o.title}</div><div class="col-2"><font color="${o.kyu>=7 ? "white" : o.kyu<7 && o.kyu>4 ? "yellow" : "red"}">${o.kyu}</font></div><div data-toggle="collapse" href="#collapse${o.id}" role="button" aria-expanded="false" aria-controls="collapseExample" class="col-4 code-drop">&lt;/&gt;</div></div><div class="row collapse" id="collapse${o.id}"> <div class="col card card-body"> <pre class="prettyprint"> <code class="language-javascript" >${code}</code> </pre></div></div>`;
