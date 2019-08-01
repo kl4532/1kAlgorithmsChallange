21617d9d6eaeb78bdd30e1cb7a0f99a3621e52b0
kl4532 <kornellukasiak@gmail.com>
2019-08-01 16:53:04 +0200

    new columns added

--- app.js
+++ app.js
@@ -12,8 +12,17 @@ fetch("challanges.json")
           code = String(code).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
           odd ? bgcolor="bg-dark" : bgcolor="bg-secondary";
           odd ? odd = false : odd = true;
-          html += `<div class="row ${bgcolor} text-light"><div class="col-2">${o.id}</div><div class="col-4">${o.title}</div><div class="col-2"><font color="${o.kyu>=7 ? "white" : o.kyu<7 && o.kyu>4 ? "yellow" : "red"}">${o.kyu}</font></div><div data-toggle="collapse" href="#collapse${o.id}" role="button" aria-expanded="false" aria-controls="collapseExample" class="col-4 code-drop">&lt;/&gt;</div></div><div class="row collapse" id="collapse${o.id}"> <div class="col card card-body"> <pre class="prettyprint"> <code class="language-javascript" >${code}</code> </pre></div></div>`;
+          html += `<div class="row ${bgcolor} text-light"><div class="col-2">${o.id}</div><div class="col-3">${o.title}</div><div class="col-2">${o.lang}</div><div class="col-2"><font color="${o.kyu>=7 ? "white" : o.kyu<7 && o.kyu>4 ? "yellow" : "red"}">${o.kyu}</font></div><div data-toggle="collapse" href="#collapse${o.id}" role="button" aria-expanded="false" aria-controls="collapseExample" class="col-3 code-drop">&lt;/&gt;</div></div><div class="row collapse" id="collapse${o.id}"><div class="col card card-body"><a onClick=fetchReadme('${o.readme}') href="#">Readme</a><pre class="prettyprint"> <code class="language-javascript">${code}</code></pre></div></div>`;
           document.getElementById("bdy").innerHTML=html;
         })
     })
   });
+  function fetchReadme(rLink) {
+    console.log(typeof(rLink));
+    fetch(rLink)
+      .then(response => response.text())
+      .then(code => {
+        alert(code);
+      })
+
+  }
